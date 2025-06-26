dd242596a42db946c4a70a4fc69884314fda17c7
Takanobu Saito <ngk@artlogic.co.jp>
2025-06-26 12:13:45 +0900

    点灯時間を0.1秒→0.2秒

--- pico_blink.ino
+++ pico_blink.ino
@@ -3,9 +3,12 @@ void setup() {
 }
 
 void loop() {
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(100);
-	digitalWrite(LED_BUILTIN, LOW);
+	for(int i = 0 ; i < 3  i++) {
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(100);
+		digitalWrite(LED_BUILTIN, LOW);
+		delay(1000);
+	}
 	delay(1000);
 }
 	
